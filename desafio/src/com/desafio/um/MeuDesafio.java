bd26e0b84c79abf7229dd90062a04f06e7d1c593
Adyellen <adyelleenalves@gmail.com>
2020-07-01 20:39:55 -0300

    first commit

---
+++ desafio/src/com/desafio/um/MeuDesafio.java
@@ -0,0 +1,14 @@
+package com.desafio.um;
+
+public class MeuDesafio {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 5;
+		int resultado = a + b;
+		System.out.println("A soma é: " + resultado);
+
+	}
+
+}
